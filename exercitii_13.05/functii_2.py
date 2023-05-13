75dd5d48afdf2a57023778ffcacffce772d12a00
Darathor12 <liviu_tanasa12@yahoo.com>
2023-05-13 09:56:54 +0300

    updated

--- exercitii_13.05/functii_2.py
+++ exercitii_13.05/functii_2.py
@@ -1,11 +1,13 @@
 # 2. Funcție care să returneze TRUE dacă un număr este par, FALSE pentru impar
 
 def check_positive(a):
-    if a >= 0:
+    if a > 0:
         # print("The number you introduced is positive!")
-        print(a.ispositive())
-    else:
+        print(True)
+    elif a < 0:
         # print("The number you introduced is negative")
-        print(a.ispositive())
+        print(False)
+    else:
+        print("Number is 0")
 
-check_positive(float(input("Introduce a number to check its parity:\n")))
+check_positive(float(input("Introduce a number to check its value:\n")))
